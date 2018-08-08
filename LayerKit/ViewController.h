c8c3c761beb9c448fabf32f04fe998efece6011d
xlsn0w <xlsn0wios@gmail.com>
2018-08-08 17:57:35 +0800

    add

---
+++ LayerKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayerKit
+//
+//  Created by HL on 2018/8/8.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LayerKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LayerKit
+//
+//  Created by HL on 2018/8/8.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
